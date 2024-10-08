19bca6664bbb5c6ef724ebfb96d8e309e32cdb87
fer_juares_coria <fernandojuarescoria@gmail.com>
2024-10-08 16:21:56 -0300

    feat: added anottation to repository

--- src/main/java/com/cdt/usermanager/repositories/Users.java
+++ src/main/java/com/cdt/usermanager/repositories/Users.java
@@ -2,8 +2,6 @@ package com.cdt.usermanager.repositories;
 
 import com.cdt.usermanager.domains.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface Users extends JpaRepository<UserData, String> {
 }
