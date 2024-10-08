5d0db8190d11ddb0aac46e7a52abbbce4a74d8a5
fer_juares_coria <fernandojuarescoria@gmail.com>
2024-10-08 16:12:26 -0300

    docs: added documentation for swagger and javadocs

--- src/main/java/com/cdt/usermanager/AppApplication.java
+++ src/main/java/com/cdt/usermanager/AppApplication.java
@@ -1,13 +1,24 @@
 package com.cdt.usermanager;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customizeOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("User Manager")
+                .version("1.0.0")
+                .description("Basic API for registering and obtaining users in Spring Boot with MySQL"));
+    }
 
 }
--- src/main/java/com/cdt/usermanager/services/UserManagerService.java
+++ src/main/java/com/cdt/usermanager/services/UserManagerService.java
@@ -8,20 +8,43 @@ import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
+/**
+ * UserManagerService is a service that manages the main logic of the Controller
+ */
+
 @Service
 public class UserManagerService {
 
     private final Users users;
 
+    /**
+     * Constructor of the service
+     *
+     * @param users is the repository of users
+     */
     @Autowired
     public UserManagerService(Users users) {
         this.users = users;
     }
 
+    /**
+     * Register a user
+     *
+     * @param userData necessary info of a user
+     * @return the complete user registered
+     * @throws DataIntegrityViolationException if the user has invalid information
+     */
     public UserData createUser(UserData userData) {
         return users.save(userData);
     }
 
+    /**
+     * Search for a user by the id
+     *
+     * @param id user identification
+     * @return the specified user
+     * @throws NoSuchElementException if the user is not found
+     */
     public UserData getUser(String id) {
         return users.findById(id).get();
     }
